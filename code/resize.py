028173f6fb04b30b7b21fb0dc1c2061417a661b2
Aertfly <123474051+Aertfly@users.noreply.github.com>
2024-05-28 16:51:20 +0200

    Ajout arg pour ignorer certains dossiers

--- code/resize.py
+++ code/resize.py
@@ -19,6 +19,21 @@ import argparse
 #     with rasterio.open(file) as tif:    
 #         img = tif.read()
 
+def isImage(pth):
+    char = pth[-1]
+    temp = ""
+    i = 1
+    while(char != "." and i<5):
+        temp += char
+        i += 1
+        char = pth[-i]
+    temp = temp[::-1]
+    return temp == "tif"
+
+def createDir(file,dirOut):
+    dir = dirOut+"/"+file.name 
+    if not(os.path.exists(dir) and os.path.isdir(dir)):
+        os.mkdir(dir)
 
 def resizeImagesSaved(dir,dirName,dirOut,ignored=[], fact=32):
     dirOut += "/"+ dirName
@@ -32,10 +47,6 @@ def resizeImagesSaved(dir,dirName,dirOut,ignored=[], fact=32):
                 resizeSaved(file,dirOut,fact)
     print(f"Fin du dir : {dirName}")
 
-def createDir(file,dirOut):
-    dir = dirOut+"/"+file.name 
-    if not(os.path.exists(dir) and os.path.isdir(dir)):
-        os.mkdir(dir)
        
 def resizeSaved(file,dirOut,fact):
     print(f"      Je vais resize {file.name} avec un facteur de {fact}")
@@ -44,17 +55,15 @@ def resizeSaved(file,dirOut,fact):
         img = cv2.resize(img, (img.shape[1]//fact, img.shape[0]//fact), interpolation=cv2.INTER_AREA)
         np.save(dirOut+"/"+"/"+file.name[0:-4],img)
 
-
-def isImage(pth):
-    char = pth[-1]
-    temp = ""
-    i = 1
-    while(char != "."):
-        temp += char
-        i += 1
-        char = pth[-i]
-    temp = temp[::-1]
-    return temp == "tif"
+def ignoreDir(initDir,strongIgnore):
+    ignore = []
+    with os.scandir(initDir) as dirs:
+        for dir in dirs:
+            if(dir.is_dir()):
+                ignore.append(dir.path)
+                if dir.name in strongIgnore :
+                    ignore += ignoreDir(dir,strongIgnore)
+    return ignore
 
 if __name__ == "__main__":
     directory_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -65,10 +74,9 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--fact", help="facteur de division de l'image originale")
-    parser.add_argument("--ignored",help="Choisir les dossiers à ignorer")
+    parser.add_argument("--ignored",help="Choisir les dossiers à ignorer de la forme dir1_dir2_dir3")
     args = parser.parse_args()
-
-
+    ignored = ignoreDir(initDir,args.ignored.split("_"))
 
     try :
         if saved :
@@ -81,11 +89,3 @@ if __name__ == "__main__":
         print(f"Permission refusée pour accéder à {initDir}.")
 
 
-def ignoreDir(initDir,strongIgnore):
-    ignore = []
-    with os.scandir(initDir) as dirs:
-        for dir in dirs:
-            ignore.append(dir.path)
-            if(dir) in strongIgnore :
-                ignore += ignoreDir(dir)
-    return ignore
