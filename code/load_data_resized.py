e73d25126e1e3268023ceacbebfb21ab9b0f3e9e
Aertfly <123474051+Aertfly@users.noreply.github.com>
2024-06-10 10:43:12 +0200

    Merge

--- code/load_data_resized.py
+++ code/load_data_resized.py
@@ -8,7 +8,7 @@ class NTLSoftLoaderResized(NTLSoftLoader):
         super().__init__(data, ntl_type)
         self.path_to_ntls = '../dataResized'
     
-    def load_ntls(self):
+    def load_ntls(self):    
         ntls = [self.load_one_ntl(2000,self.ntl_type)]
         shape = (len(ntls[0]),len(ntls[0][0]))
         for year in range(2001, 2021):
